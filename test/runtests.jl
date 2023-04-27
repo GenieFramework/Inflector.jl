8718ddb7b3b3ffed974a9ad2d0b85b574bc2e2a5
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-04-27 21:16:33 +0530

    added test for inflector

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,27 @@ using Inflector
 using Test
 
 @testset "Inflector.jl" begin
-    # Write your tests here.
+    @testset "from_dashes" begin
+        @test Inflector.from_dashes("foo-bar") == "FooBar"
+        @test Inflector.from_dashes("foo-bar-baz") == "FooBarBaz"
+    end
+
+    @testset "from_underscores" begin
+        @test Inflector.from_underscores("foo_bar") == "FooBar"
+        @test Inflector.from_underscores("foo_bar_baz") == "FooBarBaz"
+    end
+
+    @testset "to_plural" begin
+        @test Inflector.to_plural("category") == "categories"
+        @test Inflector.to_plural("story") == "stories"
+        @test Inflector.to_plural("story", is_irregular = false) == "stories"
+        @test Inflector.to_plural("thesis", is_irregular = true) == "theses"
+    end
+
+    @testset "to_singular" begin
+        @test Inflector.to_singular("categories") == "category"
+        @test Inflector.to_singular("stories") == "story"
+        @test Inflector.to_singular("stories", is_irregular = false) == "story"
+        @test Inflector.to_singular("radii", is_irregular = false) == "radius"
+    end
 end
